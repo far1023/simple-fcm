015ae7b0f691125257e8a39ce5244cbc8567634e
Fuad Agil <far1023@users.noreply.github.com>
2020-09-18 14:51:12 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,146 @@
+<html>
+
+<head>
+    <title>Firebase Cloud Messaging</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <style>
+        input[type=text],
+        select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type=submit] {
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+    </style>
+
+</head>
+
+<body>
+    <h3>Firebase Cloud Messaging</h3>
+
+    <div>
+        <form action="index.php" method="get">
+            <label for="token">FCM Registration Token</label>
+            <input type="text" id="token" name="token" placeholder="Input Token">
+
+            <label for="message">Message</label>
+            <input type="text" id="message" name="message" placeholder="Input Message">
+
+            <input type="hidden" name="type" value="single">
+            <input type="submit" value="Kirim">
+        </form>
+
+
+        <form action="index.php">
+            <label for="fname">Topics</label>
+
+            <select name="topics">
+                <option value="news">News</option>
+                <option value="promo">Promo</option>
+            </select>
+
+            <label for="lname">Message</label>
+            <input type="text" id="message" name="message" placeholder="Input Message">
+
+            <input type="hidden" name="type" value="topics">
+            <input type="submit" value="Kirim">
+        </form>
+
+    </div>
+
+    <?php
+    // Enabling error reporting
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+
+    $type = isset($_GET['type']) ? $_GET['type'] : '';
+
+    $fields = NULL;
+
+    if ($type == "single") {
+        $token = isset($_GET['token']) ? $_GET['token'] : '';
+        $message = isset($_GET['message']) ? $_GET['message'] : '';
+        $click_action = "ACTIVITY_1";
+
+        $res = array();
+        $res['body'] = $message;
+
+        $fields = array(
+            'to' => $token,
+            'notification' => $res,
+            'click_action' => $click_action,
+        );
+
+        echo 'FCM Reg Id : ' . $token . '<br/>Message : ' . $message . '<br/>Click : ' . $click_action;
+    } else if ($type == "topics") {
+        $topics = isset($_GET['topics']) ? $_GET['topics'] : '';
+        $message = isset($_GET['message']) ? $_GET['message'] : '';
+
+        $res = array();
+        $res['body'] = $message;
+
+        $fields = array(
+            'to' => '/topics/' . $topics,
+            'notification' => $res,
+        );
+
+        echo json_encode($fields);
+        echo 'Topics : ' . $topics . '<br/>Message : ' . $message;
+    }
+
+    // Set POST variables
+    $url = 'https://fcm.googleapis.com/fcm/send';
+    $server_key =   "AAAA2T9zIks:APA91bEevvifGnW2A15MAtl6fS0k6E3ZEHGMwS8xIECGprZ-OjWx81weUKN8gir1Jo1h_Sd6GtomxzIMhtZSGWoNkxe1rVf-DXBky4aQXQC6S2xtrnKkaC72zq5tkZZ2R7TAp1Mdfax2";
+
+    $headers = array(
+        'Authorization: key=' . $server_key,
+        'Content-Type: application/json'
+    );
+    // Open connection
+    $ch = curl_init();
+
+    // Set the url, number of POST vars, POST data
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Disabling SSL Certificate support temporarly
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+    // Execute post
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+        echo 'Curl failed: ' . curl_error($ch);
+    }
+
+    // Close connection
+    curl_close($ch);
+
+    ?>
+
+</body>
+
+</html>
